37eeec13fc06904f3c6a6ec96a5229c5679e6a7c
xiao <317396226@qq.com>
2019-11-12 10:46:02 +0800

    add health controller

---
+++ eureka-server/src/main/java/com/springcloud/eurekaserver/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.springcloud.eurekaserver.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/")
+public class HealthController {
+    @GetMapping(path="/health",produces= MediaType.TEXT_PLAIN_VALUE)
+    public String health(){
+        return "ok";
+    }
+}
